64729ef4cbd23e98284904c81156396da5c8a0d3
aldipee <aldipeee@gmail.com>
2020-06-02 16:13:41 +0700

    add period timeline endpoint

--- src/App/config.js
+++ src/App/config.js
@@ -15,8 +15,17 @@ export const STUDY_PROGRAMS_API = {
   update: (id: string) => `${BASE_URL}/study_programs/${id}`,
   delete: (id: string) => `${BASE_URL}/study_programs/${id}`,
 };
+
+export const TIMELINES_API = {
+  getAll: `${BASE_URL}/timelines`,
+  getSingle: (id: string) => `${BASE_URL}/study_programs/${id}`,
+  post: `${BASE_URL}/study_programs`,
+  update: (id: string) => `${BASE_URL}/study_programs/${id}`,
+  delete: (id: string) => `${BASE_URL}/study_programs/${id}`,
+};
+
 const AUTH_TOKEN =
-  'eyJhbGciOiJSUzI1NiIsImtpZCI6ImY1YzlhZWJlMjM0ZGE2MDE2YmQ3Yjk0OTE2OGI4Y2Q1YjRlYzllZWIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc2ktcmVrcnV0IiwiYXVkIjoic2ktcmVrcnV0IiwiYXV0aF90aW1lIjoxNTg5ODczMzc0LCJ1c2VyX2lkIjoidlRPdFBDdjY1MVN2Uk1oVnlYTUNkMVFNUEdzMiIsInN1YiI6InZUT3RQQ3Y2NTFTdlJNaFZ5WE1DZDFRTVBHczIiLCJpYXQiOjE1ODk4NzMzNzQsImV4cCI6MTU4OTg3Njk3NCwiZW1haWwiOiJmYWRsaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiZmFkbGlAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.ETBYutltaz_GUAdo-VT437eTiUpu_UTn0rNCg0H-3yJ1ez4wh8A2YGVlGwoaVpX2_nLxWqUSv5MccfmHgkhueLQKLxburWy0zoN4ude6q7z4fRY5LPKMq_aAYpPKQyXViyMbOIMTYFVtttughMs0rNaWZHvJnOVy90ri518QOu-AM1ZT0gtHjOGKbYlufPcg7braZbvauAfdGWEnqyY2uMUIsgAh1IKCzzv5xCANup5S2Brg8Vi6zjj8uTOQei4CE4WBnyjcK90ZzEMFO194Hx1HNuHpiKwebFZBNRYqk6gJEnvvuUNkvtVuNIbcO0ZoX5MmGQRh0KnNamDyV4-tVw';
+  'eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0Mzg3ZGUyMDUxMWNkNDgzYTIwZDIyOGQ5OTI4ZTU0YjNlZTBlMDgiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vc2ktcmVrcnV0IiwiYXVkIjoic2ktcmVrcnV0IiwiYXV0aF90aW1lIjoxNTkxMDg4MzY4LCJ1c2VyX2lkIjoidlRPdFBDdjY1MVN2Uk1oVnlYTUNkMVFNUEdzMiIsInN1YiI6InZUT3RQQ3Y2NTFTdlJNaFZ5WE1DZDFRTVBHczIiLCJpYXQiOjE1OTEwODgzNjgsImV4cCI6MTU5MTA5MTk2OCwiZW1haWwiOiJmYWRsaUBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcmViYXNlIjp7ImlkZW50aXRpZXMiOnsiZW1haWwiOlsiZmFkbGlAZ21haWwuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.IUrCe9TIg5ReK0jOA7Y1qQGXeictzrChCtlyXiz4MTmQBgCs_cAyJJr58TORHuzyh5w32_6cY7X3Dj7WaJq82GU6dv5gLz6I73-uMQVOD9GzYDXcc9U5-8QvYW_PC7Kb0ps5lBrZhpEjy2SoDCGtCk55hvCRyqPNK4i3uhEGYcAlz-LzsblWMevuCs1ovLM3Ov39CZC4DT4ajaEl9cXqo5_LFOKdW2wcd4SzZRE2H9HSeouXbGcvD2784YvkcBQBcQME8a3sx95HFgGyp-GVm_rVhP6Gjzr0fonQ-didZS6oHy_vSbL94lsTwWTArI3Tg_Q8WrZXjbSq0uOVJaFw-Q';
 
 export const config = {
   headerConfig: {
