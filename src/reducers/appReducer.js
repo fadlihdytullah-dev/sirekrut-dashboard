560faa9fe7e820deaba453518a08246fd58598c2
aldipee <aldipeee@gmail.com>
2020-06-02 16:14:29 +0700

    add timelines reducers

--- src/reducers/appReducer.js
+++ src/reducers/appReducer.js
@@ -25,11 +25,37 @@ export const initState = {
   loading: false,
   error: null,
   studyPrograms: [],
+  dataTimelines: [],
   positions: [],
 };
 
 const appReducer = (state: State, action: Action): State => {
   switch (action.type) {
+    case 'FETCH_TIMELINES_INIT': {
+      return {
+        ...state,
+        loading: true,
+      };
+    }
+
+    case 'FETCH_TIMELINES_SUCCESS': {
+      return {
+        ...state,
+        dataTimelines: action.payload.dataTimelines,
+        loading: false,
+        error: null,
+      };
+    }
+
+    case 'FETCH_TIMELINES_FAILURE': {
+      console.log('❌ error:=', action.payload.error);
+
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+      };
+    }
     case 'FETCH_STUDY_PROGRAMS_INIT': {
       return {
         ...state,
