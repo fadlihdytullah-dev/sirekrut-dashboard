36e70ab6023e87ded223bc4e2b4509eb27c091d1
Fadli Hidayatullah <fadlihdytullah.dev@gmail.com>
2020-07-21 15:58:01 +0700

    Update Table

--- src/App/Applicants/components/PassedApplicantsTable.js
+++ src/App/Applicants/components/PassedApplicantsTable.js
@@ -12,53 +12,57 @@ const PassedApplicantsTable = (props: Props) => {
   return (
     <div>
       <div>
-        <ReactHTMLTableToExcel
-          id="test-table-xls-button"
-          className="download-table-xls-button"
-          table="table-to-xls"
-          filename="pelamar"
-          sheet="sheet1"
-          buttonText="Export Data"
-        />
-        <table id="table-to-xls" className="zui-table">
-          <thead>
-            <tr>
-              <th>Nama Lengkap</th>
-              <th>Posisi</th>
-              <th>Tes Akademik</th>
-              <th>Tes Psikotes</th>
-              <th>Tes Microteaching</th>
-              <th>Tes Interview</th>
-              <th>Tes Orientasi</th>
-              <th>Email</th>
-              <th>Asal</th>
-              <th>Tanggal Lahir</th>
-              <th>Jenis Kelamin</th>
-              <th>Telepon</th>
-              <th>Pendidikan Terakhir</th>
-            </tr>
-          </thead>
-          <tbody>
-            {props.data &&
-              props.data.map((item) => (
+        {props.data && props.data.length === 0 ? null : (
+          <React.Fragment>
+            <ReactHTMLTableToExcel
+              id="test-table-xls-button"
+              className="download-table-xls-button"
+              table="table-to-xls"
+              filename="pelamar"
+              sheet="sheet1"
+              buttonText="Export Data"
+            />
+            <table id="table-to-xls" className="zui-table">
+              <thead>
                 <tr>
-                  <td>{item.fullName}</td>
-                  <td>{item.positionName}</td>
-                  <td>{item.score.academicScore || 0}</td>
-                  <td>{item.score.psikotesScore || 0}</td>
-                  <td>{item.score.microteachingScore || 0}</td>
-                  <td>{item.score.interviewScore || 0}</td>
-                  <td>{item.score.orientationScore || 0}</td>
-                  <td>{item.email}</td>
-                  <td>{item.originFrom}</td>
-                  <td>{item.dateOfBirth}</td>
-                  <td>{item.gender}</td>
-                  <td>{item.phoneNumber}</td>
-                  <td>{capitalize(item.lastEducation) || ''}</td>
+                  <th>Nama Lengkap</th>
+                  <th>Posisi</th>
+                  <th>Tes Akademik</th>
+                  <th>Tes Psikotes</th>
+                  <th>Tes Microteaching</th>
+                  <th>Tes Interview</th>
+                  <th>Tes Orientasi</th>
+                  <th>Email</th>
+                  <th>Asal</th>
+                  <th>Tanggal Lahir</th>
+                  <th>Jenis Kelamin</th>
+                  <th>Telepon</th>
+                  <th>Pendidikan Terakhir</th>
                 </tr>
-              ))}
-          </tbody>
-        </table>
+              </thead>
+              <tbody>
+                {props.data &&
+                  props.data.map((item) => (
+                    <tr>
+                      <td>{item.fullName}</td>
+                      <td>{item.positionName}</td>
+                      <td>{item.score.academicScore || 0}</td>
+                      <td>{item.score.psikotesScore || 0}</td>
+                      <td>{item.score.microteachingScore || 0}</td>
+                      <td>{item.score.interviewScore || 0}</td>
+                      <td>{item.score.orientationScore || 0}</td>
+                      <td>{item.email}</td>
+                      <td>{item.originFrom}</td>
+                      <td>{item.dateOfBirth}</td>
+                      <td>{item.gender}</td>
+                      <td>{item.phoneNumber}</td>
+                      <td>{capitalize(item.lastEducation) || ''}</td>
+                    </tr>
+                  ))}
+              </tbody>
+            </table>
+          </React.Fragment>
+        )}
       </div>
     </div>
   );
